e966a7de0c9c3e8ffbb4ff63f3ce61673a080fdf
Stanislav Kostenich <52426715+ukrsite@users.noreply.github.com>
2023-12-17 22:48:37 +0000

    update maintf

--- main.tf
+++ main.tf
@@ -37,22 +37,8 @@ provider "flux" {
   }
 }
 
-output "host" {
-  value = kind_cluster.this.endpoint
-}
-output "client_certificate" {
-  value = kind_cluster.this.client_certificate
-}
-output "client_key" {
-  value = kind_cluster.this.client_key
-}
-output "cluster_ca_certificate" {
-  value = kind_cluster.this.cluster_ca_certificate
-}
-
-
 resource "flux_bootstrap_git" "this" {
-  # depends_on = [github_repository_deploy_key.this]
+  depends_on = [github_repository_deploy_key.this]
 
-  # path = "clusters/my-cluster"
+  path = "clusters/my-cluster"
 }
